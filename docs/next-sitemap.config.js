eed9cca1754366f4123f14e53c8b1379e8aa197a
Marc Klingen <git@marcklingen.com>
2025-02-09 03:34:38 +0100

    feat: add sitemap, robots.txt and llms.txt

---
+++ docs/next-sitemap.config.js
@@ -0,0 +1,18 @@
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://oss-llmops-stack.com',
+    generateRobotsTxt: true,
+    changefreq: 'daily',
+    additionalPaths: async (config) => [{
+        loc: '/',
+        priority: 1,
+        changefreq: 'daily',
+        lastmod: new Date().toISOString()
+    }],
+    exclude: [
+        // Exclude _meta files
+        '*/_meta',
+        '/events/*'
+    ],
+}
